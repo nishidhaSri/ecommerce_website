8c04921bba84b80122711668450a05f743914938
Nishidha Sri <44313138+MeekMo@users.noreply.github.com>
2020-07-07 15:06:18 +0530

    removed redux logger from heroku build

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,13 @@ import rootReducer from "./root-reducer";
 //we defined middleware as an array so that we can keep adding
 //middlewares which we want and the spread component
 //enables all the middleware present
-const middleware = [logger];
+const middleware = [];
+
+//As we dont want our deployed website to have the logger, we do the below =>
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
